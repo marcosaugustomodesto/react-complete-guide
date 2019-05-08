45391978b3cd50e829f2b9f95cb4bf667ece184e
Modesto <msmd@gft.com>
2019-05-08 15:27:32 -0300

    static content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1>Hi, I'm a React App</h1>
+        <h1>Hi, I'm a React App</h1>
+        <p>this is really working!</p>
+        <Person />
+        <Person />
+        <Person />
       </div>
     );
   }
