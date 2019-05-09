67be6c12f023643a88e5768291528e41fc449872
Modesto <msmd@gft.com>
2019-05-09 14:36:08 -0300

    flexible lists

--- src/App.js
+++ src/App.js
@@ -13,6 +13,22 @@ class App extends Component {
     showPersons: false
   }
 
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {...this.state.persons[personIndex]};
+
+    person.name = event.target.value;
+
+    const persons = {...this.state.persons};
+
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
+  }
+
   deletePersonHandler = (personIndex) => {
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
@@ -41,9 +57,11 @@ class App extends Component {
           {this.state.persons.map((person, index) =>{
              return <Person
                 click={() => this.deletePersonHandler(index)}             
+                changed={(event) => this.nameChangedHandler(event, person.id)}
                 name={person.name} 
                 age={person.age}
-                key={person.id}/>             
+                key={person.id}
+                />             
           })}
         </div> 
       )
